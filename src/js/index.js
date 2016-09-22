70ae26ccb5b76894cedc34025cc8bf8529c31e97
matheusmessora <matheus.messora.vpn@gmail.com>
2016-09-22 00:56:10 -0300

    improving call-to-action

--- src/js/index.js
+++ src/js/index.js
@@ -19,16 +19,34 @@ var INDEX = (function () {
                     $("#participant-form-row").hide('fast');
                     $("#participant-registered-text").show('fast');
                 });
-
             }
         }
 
         $('#participate').click(doParticipate);
     }
 
+    /**
+     * Credits to https://css-tricks.com/snippets/jquery/smooth-scrolling/
+     */
+    function bindAnchors(){
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    }
+
 
     function init() {
         bindSendButton();
+        bindAnchors();
     }
 
     return {
