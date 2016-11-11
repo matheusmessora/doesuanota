a6e293a35609cab8d7b8ed051cecdf2f46c3a137
matheusmessora <matheus.messora.vpn@gmail.com>
2016-11-11 15:20:16 -0200

    mehorias de texto

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,7 @@
 var CONFIG = (function () {
 
     return {
-        apiDomain: "http://api.doesuanota.com.br/"
+        apiDomain: "http://localhost:9090/"
+        // apiDomain: "http://api.doesuanota.com.br/"
     };
 }());
